e06281f5cd0c3f97848043201cb67b2f5ac5fe70
junghim <46070571+junghim@users.noreply.github.com>
2021-03-14 17:58:19 +0900

    Create pytest.py

---
+++ pytest.py
@@ -0,0 +1,102 @@
+import discord
+import requests
+from bs4 import BeautifulSoup
+from discord.ext import commands
+import load_json_variable as variable
+import json
+import re
+bot = commands.Bot(command_prefix='$')
+
+@bot.event
+async def on_ready():
+
+    game = discord.Game("TESTING")
+    await bot.change_presence(status=discord.Status.online, activity=game)
+    print("READY")
+@bot.event
+async def on_message(message):
+    # 봇이 메시지를 보낸 경우 어떠한 작업도 하지 않음.
+    if message.author.bot:
+        return None
+    if message.content[0:4] == "$검색 ":
+        messagesplit = message.content.split(" ")
+        messagesplit = ''.join(messagesplit[1:])
+        webpage = requests.get("https://www.google.com/search?q="+str(messagesplit)+"&hl=ko&tbm=isch")
+        soup = BeautifulSoup(webpage.text, "html.parser")
+        img = soup.find_all('img')
+        img2=img[2]
+        img_src=img2.get('src')
+        embed = discord.Embed(title=message.content[4:]+":",
+                              description="https://www.google.com/search?q="+str(messagesplit)+"&hl=ko&tbm=isch",
+                              color=0x383b38)
+        embed.set_footer(
+            icon_url="https://www.google.com/search?q="+str(messagesplit)+"&hl=ko&tbm=isch")
+        embed.set_image(url=img_src)
+        await message.channel.send(message.content, embed=embed)
+    if message.content[0:4] == "$계산 ":
+        splitphase = message.content.split(" ")
+        first1 = int(splitphase[1])
+        second2 = splitphase[2]
+        third3 = int(splitphase[3])
+        result2 = 0
+        if second2 == "*":
+            result2 = first1 * third3
+        elif second2 == "/":
+            result2 = first1 / third3
+        elif second2 == "+":
+            result2 = first1 + third3
+        elif second2 == "-":
+            result2 = first1 - third3
+        await message.channel.send(result2)
+    if message.content[0:5] == "$계산기 ":
+        splitphase = message.content.split(" ")
+        result3 = int(splitphase[1])
+
+        for i in range(len(splitphase)):
+            if i == 0 or i == 1:
+                pass
+            elif splitphase[i] != "*" and splitphase[i]!="+"and splitphase[i]!= "/"and splitphase[i]!= "-":
+                pass
+            elif splitphase[i] == "*":
+                result3 *= int(splitphase[i+1])
+            elif splitphase[i] == "+":
+                result3 += int(splitphase[i+1])
+            elif splitphase[i] == "-":
+                result3 -= int(splitphase[i+1])
+            elif splitphase[i] == "/":
+                result3 /= int(splitphase[i+1])
+        await message.channel.send(result3)
+    if message.content[0:4] == "$삭제 ":
+        split4 = message.content.split(" ")
+        split4 = int(split4[1])
+        await message.channel.purge(limit=split4)
+    if message.content[0:4] == "$도배 ":
+        split5 = message.content.split(" ")
+        split6 = int(split5[1])
+        split7 = ' '.join(split5[2:])
+        for i in range(split6):
+            await message.channel.send(split7)
+    if message.content[0:4] == "$번역 ":
+        url2 = 'https://papago.naver.com/'
+        q = message.content[4:]
+
+        data2 = {'source': 'en',
+                'target': 'ko',
+                 'text': ''}
+        data2['text'] = q
+
+
+        html = requests.post('https://papago.naver.com/', data=data2).text
+        response = json.loads(html)
+        dict = response['message']['result']['translatedText']
+
+        await message.channel.send(q)
+
+        await message.channel.send(dict)
+
+
+
+
+if __name__ == "__main__":
+
+    bot.run(variable.get_token())
